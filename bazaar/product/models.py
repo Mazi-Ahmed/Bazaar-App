5325afce757bafe96dd36ee37a19c1fb623e69b9
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-09-10 20:00:41 -0400

    Created Model: Product

---
+++ bazaar/product/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+        def __str__(self):
+            return self.name 
+        
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    item_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE) 
+    created_date = models.DateTimeField(auto_now_add=True)
