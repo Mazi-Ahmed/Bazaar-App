e00ad872203951d7c9ca822fb625be68ab756b1e
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-09-14 02:50:30 -0400

    Home Page: Recent Items and Categories

--- bazaar/bazaar/settings.py
+++ bazaar/bazaar/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'bazaar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bazaar',
     }
 }
 
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- bazaar/core/views.py
+++ bazaar/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from product.models import Category, Product
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.filter(item_sold=False)[0:5]
+    categories = Category.objects.all()
+    
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'products': products,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
