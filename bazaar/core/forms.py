7487f9254478006d3df5a31f945b54bebeadab2c
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-09-17 01:45:29 -0400

    Added User Authentication

--- bazaar/core/forms.py
+++ bazaar/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class Signup(UserCreationForm):
@@ -8,18 +8,28 @@ class Signup(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
     
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Enter username',
+        'placeholder': 'Your username',
         'class': 'w-full py-2 px-6 rounded-xl'
     }))
     email = forms.CharField(widget=forms.EmailInput(attrs={
-        'placeholder': 'Enter email',
+        'placeholder': 'Your email',
         'class': 'w-full py-2 px-6 rounded-xl'
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter password',
+        'placeholder': 'Input password',
         'class': 'w-full py-2 px-6 rounded-xl'
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter password again',
+        'placeholder': 'Input password again',
+        'class': 'w-full py-2 px-6 rounded-xl'
+    }))
+
+class Login(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-2 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Input password',
         'class': 'w-full py-2 px-6 rounded-xl'
     }))
