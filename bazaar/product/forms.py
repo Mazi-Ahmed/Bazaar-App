9f09fddc6547489fe788316ba7e08478adecf67a
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-09-17 02:46:27 -0400

    User can sell new product

---
+++ bazaar/product/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Product
+
+class NewProduct(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'category', 'price', 'image')
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 rounded-xl border'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full py-2 px-4 rounded-xl border'
+            }),
+            'category': forms.Select(attrs={
+                'class': 'w-full py-2 px-4 rounded-xl border'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 rounded-xl border'
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'w-full py-2 px-4 rounded-xl border'
+            })}
--- bazaar/product/views.py
+++ bazaar/product/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Product
-
+from .forms import NewProduct
 
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -8,4 +9,20 @@ def detail(request, pk):
     return render(request, 'product/detail.html', {
         'product': product,
         'similar_products': similar_products
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewProduct(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.created_by = request.user
+            product.save()
+            return redirect('product:detail', pk=product.id)
+    else:
+        form = NewProduct()
+    return render(request, 'product/form.html', {
+        'form': form,
+        'title': 'New Product'
     })
