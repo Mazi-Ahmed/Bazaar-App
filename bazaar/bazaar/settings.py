c118d2dce3356a748281c0e36de3b38e3876cfd0
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-10-02 19:09:23 -0400

    Config deployment

--- bazaar/bazaar/settings.py
+++ bazaar/bazaar/settings.py
@@ -135,3 +135,7 @@ MEDIA_ROOT = 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
